80e509db96f730953ca608378aaf094621bf20fe
el.beress@gmail.com <el.beressa@gmail.com>
2021-02-19 11:05:30 +0100

    la carte avec markers

--- src/components/HotelMap.jsx
+++ src/components/HotelMap.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map, TileLayer, Marker, Circle, Popup } from "react-leaflet"
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import icone from "./icon.png"
+import L from "leaflet"
+
+let imageIcon = L.icon({
+  iconUrl: icone,
+  iconSize: [38, 45], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+
 
 class HotelMap extends React.Component {
   constructor() {
     super();
     this.state = {
-      nimportequoi: ["HOURRRRRRAAAAA"],
+      nimportequoi: [],
     };
   }
 
@@ -16,15 +29,24 @@ class HotelMap extends React.Component {
     console.log(this.props.center)
     return (
       <div id="mapid">
-        <h1>HotelMap</h1>
+        <h1>HotelMap  </h1>
         <Map
-          style={{ width: 400, height: 400 }}
+          style={{ width: 800, height: 400 }}
           center={[this.props.center.lat, this.props.center.lon]} zoom={13} scrollWheelZoom={false}>
+
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
 
+
+          />
+          <Circle center={[this.props.center.lat, this.props.center.lon]} fillColor="green" radius={1000} />
+          {this.props.hotel.map((item, index) => {
+            console.log("ee", item.location.lat)
+            return (<Marker icon={imageIcon} position={[item.location.lat, item.location.lon]}           >
+
+            </Marker>)
+          })}
 
         </Map>
       </div>
--- src/view/Hotels.jsx
+++ src/view/Hotels.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import HotelMap from "./../components/HotelMap";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import HotelCard from "../components/HotelCard";
+import HotelMap from "./../components/HotelMap";
 
 class Hotels extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       cities: [],
-      center:{
-        lat:0,
-        lon:0
+      hotelDetails: [],
+      img: "http://localhost:3002",
+      center: {
+        lat: 0,
+        lon: 0
       }
     };
   }
@@ -23,11 +26,11 @@ class Hotels extends React.Component {
       `http://localhost:3002/api/hotels/city/${this.props.match.params.city}`
     )
       .then((response) => response.json())
-      .then((commodities) => {
-        console.log("commodities", commodities);
+      .then((results) => {
+        console.log("results", results);
         this.setState({
-          cities: commodities.results,
-          center:commodities.center,
+          hotelDetails: results.results,
+          center: results.center
         });
       })
       .catch((error) => console.error(error));
@@ -35,10 +38,36 @@ class Hotels extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Hotels</h1>
-         <HotelMap  center={this.state.center} hotel={this.state.cities}/>
+      <div className='container d-flex flex-column justify-content-center'>
+        <div>
+          <HotelMap center={this.state.center} hotel={this.state.hotelDetails} />
+        </div>
+
+        <h1 className='text-center tabTitle'>City details</h1>
+
+        {this.state.hotelDetails.length > 0 &&
+          this.state.hotelDetails.map((hotel) => {
+            console.log(hotel);                                                                                                                                                                                                                                                                             
+            return (
+
+              <Link to={`/hotel/${hotel._id}`}>
+                <HotelCard
+                  name={hotel.name}
+                  image={hotel.pictures[0]}
+                  //image={"http://via.placeholder.com/300x200"}
+                  alt={`${hotel.name}`}
+                  price={hotel.price}
+                  stars={hotel.stars}
+
+                />
+              </Link>
+
+            );
+
+
+          })}
       </div>
+
     );
   }
 }
